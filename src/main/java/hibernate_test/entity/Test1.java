6576bce37d5bd22e62fe5229f93f505716245b73
Rustam Osmanov <rustam-osmanov@yandex.ru>
2026-05-24 19:55:01 +0300

    Hibernate

--- src/main/java/hibernate_test/entity/Test1.java
+++ src/main/java/hibernate_test/entity/Test1.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("test1");
@@ -21,10 +23,61 @@ public class Test1 {
             System.out.println("Insert planet: " + planet); */
 
             //Select
+            System.out.println("--------------------------------------" );
             session.beginTransaction();
             Planet planet = session.get(Planet.class, 1);
             session.getTransaction().commit();
             System.out.println("Select planet: " + planet);
+
+            //Select v2
+            System.out.println("--------------------------------------" );
+            session.beginTransaction();
+            String planetName = session.createQuery("Select name from Planet where name like 'Test%'").list().toString();
+            System.out.println("Select toString planetName: " + planetName);
+            session.getTransaction().commit();
+
+
+
+            //HQL
+            System.out.println("--------------------------------------" );
+            session.beginTransaction();
+            List<Planet> planets = session.createQuery("from Planet where name like 'Test%'").getResultList();
+            for (Planet lplanet : planets) {
+                System.out.println(lplanet);
+            }
+            session.getTransaction().commit();
+
+            //UPDATE
+            System.out.println("--------------------------------------" );
+            session.beginTransaction();
+            planet = session.get(Planet.class, 111);
+            planet.setName("Test22");
+            planet.setDistance(155.50);
+            session.getTransaction().commit();
+            System.out.println("UPDATE planet: " + planet);
+
+            //Update v2
+            System.out.println("--------------------------------------" );
+            session.beginTransaction();
+            session.createQuery("update from Planet set distance = 200.00 where id = 112").executeUpdate();
+            session.getTransaction().commit();
+
+            //DELETE
+           /* System.out.println("--------------------------------------" );
+            session.beginTransaction();
+            planet = session.get(Planet.class, 113);
+            session.delete(planet);
+            session.getTransaction().commit(); */
+
+            //DELETE v2
+            System.out.println("--------------------------------------" );
+            session.beginTransaction();
+            session.createQuery("delete from Planet where id = 113").executeUpdate();
+            session.getTransaction().commit();
+
+
+
+
 
         } finally {
             sessionFactory.close();
